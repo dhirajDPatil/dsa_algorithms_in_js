f277596710b4e5c0f5aef60a06f39aee7ecd48c4
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2025-02-10 09:11:16 +0530

    bubble sort with example

--- Algorithms/Sorting.js
+++ Algorithms/Sorting.js
@@ -1,1 +1,19 @@
 // Different sorting methods of array
+let a = [3, 1, 7, 2, 0, 9, 5];
+
+// quick sort
+// merge sort
+
+// bubble sort
+for(let i=0; i<a.length-1; i++) {
+    for(let j=0; j< a.length-i-1; j++) {
+        if(a[j]>a[j+1]){
+            // swap elements
+            let temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
+        }
+    }
+}
+
+console.log('bubble sorted array ', a);
